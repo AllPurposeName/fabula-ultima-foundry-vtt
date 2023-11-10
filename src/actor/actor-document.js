8b0ea4da38c56c54721adc5e5b095f2043ee5b4c
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2023-11-09 21:07:51 -0700

    Add attrs to actor document for surfacing to ui

--- src/actor/actor-document.js
+++ src/actor/actor-document.js
@@ -1,13 +1,26 @@
 import localize from "@utils/localize-string.js";
 
 export class FabulaUltimaActor extends Actor {
-	get actorProperties() {
-		return this.system;
-	}
+  get actorProperties() {
+    return this.system;
+  }
 
-	get attributes() {
-		return this.actorProperties.baseAttribute;
-	}
+  get attributes() {
+    return this.actorProperties.baseAttribute;
+  }
+
+  get hp() {
+    return this.attributes.baseHitPoints.value
+  }
+
+  get mp() {
+    return this.attributes.baseMindPoints.value
+  }
+
+  get ip() {
+    return this
+    return this.attributes.baseInventoryPoints.value
+  }
 
   get dexterity() {
     return this.modifiedAttribute({baseAttribute: "baseDexterity", possibleStatusEffects: ["slow", "enraged"]});
@@ -31,9 +44,9 @@ export class FabulaUltimaActor extends Actor {
     return Math.max(6, baseValue - (2 * activeStatusEffects));
   }
 
-	get conditions() {
-		return this.actorProperties.condition;
-	}
+  get conditions() {
+    return this.actorProperties.condition;
+  }
 
   get bio() {
     return this.actorProperties.bio;
@@ -61,9 +74,23 @@ export class FabulaUltimaActor extends Actor {
 // 	get unlimitedPush() {
 // 		return this.getFlag("fabula-ultima", "unlimitedPush") ?? false;
 // 	}
-//
-// 	/* Override */
-// 	getRollData() {
+  //
+  /* Override */
+  getRollData() {
+    return {
+      dexterity: this.dexterity,
+      insight: this.insight,
+      might: this.might,
+      willpower: this.willpower,
+      dex: `1d${this.dexterity}`,
+      ins: `1d${this.insight}`,
+      mig: `1d${this.might}`,
+      wlp: `1d${this.willpower}`,
+      hp: this.hitPoints,
+      mp: this.mindPoints,
+      ip: this.inventoryPoints,
+    }
+  }
 // 		return {
 // 			alias: this.token?.name || this.name,
 // 			actorId: this.id,
